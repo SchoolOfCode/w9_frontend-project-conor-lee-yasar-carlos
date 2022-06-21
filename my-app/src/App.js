bcd72400678cc2c95b189b5e506326083c74c18d
leeconroy <leeconroy1977@yahoo.co.uk>
2022-06-21 09:15:08 +0100

    Nav-bar completed

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './Componants/Nav-bar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Bootcamper Organiser</h1>
+      <div className='title-underline'></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Componants/Nav-bar/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav >
+      <div className='nav-left'>
+        <div className='logo'><img src="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png" alt="" /></div>
+        <div className='logo-text'>The Free Coding Bootcamp</div>
+      </div>
+      <div className='nav-right'>
+        <ul>
+          <li>Home</li>
+          <li>Intensive Bootcamp</li>
+          <li>Sponsors & Partners</li>
+          <li>Graduates</li>
+          <li>Careers</li>
+          <li>Blog</li>
+          <button className='nav-button'>Log out</button> 
+        </ul>   
+      </div>
+
+    </nav>
+  )
+}
+
+export default NavBar
