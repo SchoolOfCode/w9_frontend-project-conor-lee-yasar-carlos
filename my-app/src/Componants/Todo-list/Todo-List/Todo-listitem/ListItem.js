308c30028331221119a65615a84dcba50768035f
ConorG1247 <conorgoddard1247@gmail.com>
2022-06-22 09:55:35 +0100

    Star functionality

--- my-app/src/Componants/Todo-list/Todo-List/Todo-listitem/ListItem.js
+++ my-app/src/Componants/Todo-list/Todo-List/Todo-listitem/ListItem.js
@@ -1,26 +1,5 @@
-// import React from 'react'
-// import { FiPlusCircle } from 'react-icons/fi'
-// import { AiOutlineCheckCircle, AiFillStar } from 'react-icons/ai'
-
-
-// function ListItem({id, listData, weekendClick}) {
-
-//   return (
-//     <div className='todo-list-item'>
-//       <p className='list-item-text'>{listData}</p>
-//       <div className='icon-box' >
-//         <span className='plus-icon'><AiOutlineCheckCircle id={id}/></span>
-//         <span className='plus-icon'><AiFillStar id={id}/></span>
-//         <span className='plus-icon'><FiPlusCircle id={id} data-txt={listData} onClick={weekendClick}/></span>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default ListItem
-
 import React from 'react'
-import  StarRating  from "./Star-rating/Star-rating"
+import StarRating from '../StarRating'
 import { FiPlusCircle } from 'react-icons/fi'
 import { AiOutlineCheckCircle } from 'react-icons/ai'
 import { BsChevronCompactDown } from 'react-icons/bs'
