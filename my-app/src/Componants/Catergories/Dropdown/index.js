4b6ec2acf9d81fc09c159d8107e246cda471715d
ConorG1247 <conorgoddard1247@gmail.com>
2022-06-22 09:28:13 +0100

    Added dropdown

--- my-app/src/Componants/Catergories/Dropdown/index.js
+++ my-app/src/Componants/Catergories/Dropdown/index.js
@@ -1,31 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 
-function Dropdown({ setDropdownData }) {
-  const statusHandler = (e) => {
-    console.log(e.target.value);
-    setDropdownData(e.target.value);
-  };
-
+function Dropdown({ selected, setSelected}) {
+  const [isActive, setIsActive] = useState(true)
+  const options = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12" ]
+  
   return (
-    <form >
-      <div>
-        <select onChange={statusHandler} className="select-week">
-          <option className="select-week" value="1">Week 1</option>
-          <option className="select-week" value="2">Week 2</option>
-          <option className="select-week" value="3">Week 3</option>
-          <option className="select-week" value="4">Week 4</option>
-          <option className="select-week" value="5">Week 5</option>
-          <option className="select-week" value="6">Week 6</option>
-          <option className="select-week" value="7">Week 7</option>
-          <option className="select-week" value="8">Week 8</option>
-          <option className="select-week" value="9">Week 9</option>
-          <option className="select-week" value="10">Week 10</option>
-          <option className="select-week" value="11">Week 11</option>
-          <option className="select-week" value="12">Week 12</option>
-        </select>
-      </div>
-    </form>
-  );
-}
 
+      <div className='dropdown'>
+        <div className='dropdown-button' onClick={e => setIsActive(!isActive)}>{selected}</div>
+        {isActive && (
+          <div className='dropdown-content'>
+            {options.map((option) => (
+              <div
+              onClick={(e) => {
+              setSelected(option);
+              setIsActive(false);
+              }}
+              className="dropdown-item">
+              {option}
+              </div>
+            ))}
+          
+          </div>
+        )
+        }
+      </div>
+  )}
+  
 export default Dropdown;
