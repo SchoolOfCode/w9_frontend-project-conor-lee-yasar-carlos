ace85ebbff66f36179d1d92d3ce01b66bc98db43
ConorG1247 <conorgoddard1247@gmail.com>
2022-06-22 09:51:13 +0100

    Adding star

--- my-app/src/Componants/Todo-list/Todo-List/Todo-listitem/ListItem.js
+++ my-app/src/Componants/Todo-list/Todo-List/Todo-listitem/ListItem.js
@@ -1,19 +1,51 @@
+// import React from 'react'
+// import { FiPlusCircle } from 'react-icons/fi'
+// import { AiOutlineCheckCircle, AiFillStar } from 'react-icons/ai'
+
+
+// function ListItem({id, listData, weekendClick}) {
+
+//   return (
+//     <div className='todo-list-item'>
+//       <p className='list-item-text'>{listData}</p>
+//       <div className='icon-box' >
+//         <span className='plus-icon'><AiOutlineCheckCircle id={id}/></span>
+//         <span className='plus-icon'><AiFillStar id={id}/></span>
+//         <span className='plus-icon'><FiPlusCircle id={id} data-txt={listData} onClick={weekendClick}/></span>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default ListItem
+
 import React from 'react'
+import  StarRating  from "./Star-rating/Star-rating"
 import { FiPlusCircle } from 'react-icons/fi'
-import { AiOutlineCheckCircle, AiFillStar } from 'react-icons/ai'
+import { AiOutlineCheckCircle } from 'react-icons/ai'
+import { BsChevronCompactDown } from 'react-icons/bs'
 
 
 function ListItem({id, listData, weekendClick}) {
-
   return (
-    <div className='todo-list-item'>
-      <p className='list-item-text'>{listData}</p>
-      <div className='icon-box' >
-        <span className='plus-icon'><AiOutlineCheckCircle id={id}/></span>
-        <span className='plus-icon'><AiFillStar id={id}/></span>
-        <span className='plus-icon'><FiPlusCircle id={id} data-txt={listData} onClick={weekendClick}/></span>
+    <div className='comlete-list-item'>
+      <div className='completed'>
+          <span className='tick-icon'><AiOutlineCheckCircle  id={id} onClick={weekendClick}/></span>
+      </div>
+      <div className='todo-list-item'>
+        <div className='todo-list-content'>
+          <p className='list-item-text'>{listData}</p>
+          <div className='icon-box' >    
+            <StarRating />         
+          </div>
+        </div>
+        <span className='chevron-icon'><BsChevronCompactDown /></span>
+      </div>
+      <div className='add-to-weekend'>
+          <span className='plus-icon'><FiPlusCircle  id={id} onClick={weekendClick}/></span>
       </div>
     </div>
+    
   )
 }
 
