be6d9fdf8b7b6e8cf622a73286aae3cc1845c8bd
ConorG1247 <conorgoddard1247@gmail.com>
2022-06-23 10:46:05 +0100

    Added completed button check

--- my-app/src/Componants/Todo-list/Todo-List/Todo-listitem/index.js
+++ my-app/src/Componants/Todo-list/Todo-List/Todo-listitem/index.js
@@ -1,32 +1,57 @@
-import React from 'react'
-import StarRating from '../StarRating'
-import { FiPlusCircle } from 'react-icons/fi'
-import { AiOutlineCheckCircle } from 'react-icons/ai'
-import { BsChevronCompactDown } from 'react-icons/bs'
+import React from "react";
+import StarRating from "../StarRating";
+import { FiPlusCircle } from "react-icons/fi";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { BsChevronCompactDown } from "react-icons/bs";
+import { useState } from "react";
 
+function ListItem({
+  id,
+  taskName,
+  weekend,
+  completed,
+  rating,
+  weekendClick,
+  updateStarRating,
+}) {
 
-function ListItem({id, taskName, weekend, completed, rating, weekendClick, updateStarRating}) {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className='comlete-list-item'>
-      <div className='completed'>
-          <span className='tick-icon'><AiOutlineCheckCircle  id={id} onClick={weekendClick}/></span>
-      </div>
-      <div className='todo-list-item'>
-        <div className='todo-list-content'>
-          <p className='list-item-text'>{taskName}</p>
-          <div className='icon-box' >
-            <StarRating taskRating={rating} updateStarRating={updateStarRating} taskId={id} />         
+    <div className="comlete-list-item">
+      <button className="completed" onClick={() => setActive(!active)} id={id}>
+        <span className="tick-icon">
+          <AiOutlineCheckCircle id={id} />
+        </span>
+      </button>
+      <div className="todo-list-item" style={{ backgroundColor: active ? "grey" : "" }} >
+        <div className="todo-list-content"  >
+          <p className="list-item-text">{taskName}</p>
+          <div className="icon-box">
+            <StarRating
+              taskRating={rating}
+              updateStarRating={updateStarRating}
+              taskId={id}
+            />
           </div>
         </div>
-        <span className='chevron-icon'><BsChevronCompactDown /></span>
+        <span className="chevron-icon">
+          <BsChevronCompactDown />
+        </span>
       </div>
-      <button className='add-to-weekend' id={id} data-txt={taskName} onClick={weekendClick}>
-          <span className='plus-icon'><FiPlusCircle  id={id} data-txt={taskName} onClick={weekendClick}/></span>
+      <button
+        className="add-to-weekend"
+        id={id}
+        data-txt={taskName}
+        onClick={weekendClick}
+        disabled={active}
+      >
+        <span className="plus-icon">
+          <FiPlusCircle id={id} data-txt={taskName} onClick={weekendClick} />
+        </span>
       </button>
     </div>
-    
-  )
+  );
 }
 
-export default ListItem
-
+export default ListItem;
