47fbe9772b6096c65b828bc5ef4082985912145f
Carlos E Alford <carlos.alford@gmail.com>
2022-06-28 16:41:56 +0100

    Comment for the DayButton component

--- my-app/src/Components/Main/Categories/DayButton/DayButton.js
+++ my-app/src/Components/Main/Categories/DayButton/DayButton.js
@@ -3,19 +3,19 @@ import { BsFillStarFill } from 'react-icons/bs'
 
 function DayButton({id, handleClick, dayData}) {
   
-  // work out the average for the day according to the tasks rating
+  // work out the average for the day according to the days tasks rating
   let average = 0;
   let result = dayData.reduce((pValue, cValue) => (pValue += cValue.rating), 0);
   average = +(result / dayData.length).toFixed(2);
 
   return (
     <div className='category-button-container'>
-    <button id={id} className='day-one-button category-button' onClick={handleClick}>Day {id} </button>
-    <div className='day-rating'>
-       <div className='category-button-star'><BsFillStarFill/></div>
-       <div className='category-button-rating'>{average}</div>
+      <button id={id} className='day-one-button category-button' onClick={handleClick}>Day {id} </button>
+      <div className='day-rating'>
+        <div className='category-button-star'><BsFillStarFill/></div>
+        <div className='category-button-rating'>{average}</div>
+      </div>
     </div>
- </div>
   )
 }
 
