763ebc31b135cd35a4eeb145e73f473203051c17
ConorG1247 <conorgoddard1247@gmail.com>
2022-06-28 15:03:13 +0100

    Refactored Categories code

--- my-app/src/Components/Categories/Dropdown/Dropdown.js
+++ my-app/src/Components/Categories/Dropdown/Dropdown.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import { useState } from 'react'
 
-function Dropdown({ selected, setSelected, setInput}) {
+function Dropdown({ updateWeekNumber }) {
 // function Dropdown({ setInput }) {
 //   const statusHandler = (e) => {
 //     console.log(e.target.value);
 //     setInput(e.target.value);
 //   };
-
+  const [selected, setSelected] = useState("Choose Week")
   const [isActive, setIsActive] = useState(true)
   const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   
   return (
-
       <div className='dropdown'>
         <div className='dropdown-button' onClick={e => setIsActive(!isActive)}>{selected}</div>
         {isActive && (
           <div className='dropdown-content'>
-            {options.map((option) => (
+            {options.map((option, index) => (
               <div
+              key={index}
+              id={option}
               onClick={(e) => {
+              updateWeekNumber(e);
               setSelected("Week " + option);
-              setInput(option)
               setIsActive(false);
               }}
               className="dropdown-item">
               {"Week " + option}
               </div>
             ))}
-          
           </div>
         )
         }
--- my-app/src/Components/Main/Main.js
+++ my-app/src/Components/Main/Main.js
@@ -1,25 +1,22 @@
 import { useState, useEffect } from "react";
-import Categories from "../Categories"
+import Categories from "../Categories/Categories";
 import Header from "./Header/Header";
-import Todo from "../Todo-list"
-import Dropdown from "../Categories/Dropdown";
+import Todo from "../Todo-list";
 import WeekendButton from "../Categories/Weekend-button";
 
-function Main({imageVisibility}) {
+function Main({ imageVisibility }) {
   const [toDoClass, setToDoClass] = useState("hidden");
   const [dayId, setDayId] = useState(0);
   const [weekendCheck, setWeekendCheck] = useState(false);
-  const [input, setInput] = useState(1)
-  const [selected, setSelected] = useState("Choose Week")
-
-
   const [weekendListData, setWeekendListData] = useState([]);
+  const [weekNumber, setWeekNumber] = useState(1);
 
   const [dayData, setDayData] = useState([
-    { list: [{ id: 1, topic: "" }], resources: [{ id: 1, topic: "", url: "", rating: 2 }] },
+    {
+      list: [{ id: 1, topic: "" }],
+      resources: [{ id: 1, topic: "", url: "", rating: 2 }],
+    },
   ]);
-
-
 
   // day button click changes div classes to visible and sets weekend check to false and sets the day id
   function handleClick(e) {
@@ -28,13 +25,14 @@ function Main({imageVisibility}) {
     setToDoClass("visible");
   }
 
+  function updateWeekNumber(e) {
+    setWeekNumber(e.target.id);
+  }
+
   // Adds a list item to the weekend list
-  function weekendAddClick({taskId, topic}) {
-    setWeekendListData([
-      ...weekendListData,
-      { id: taskId, topic: topic },
-    ]);
-    updateWeekendTask(taskId)
+  function weekendAddClick({ taskId, topic }) {
+    setWeekendListData([...weekendListData, { id: taskId, topic: topic }]);
+    updateWeekendTask(taskId);
   }
 
   // weekend button click changes div classes to visible and sets check to true
@@ -46,118 +44,158 @@ function Main({imageVisibility}) {
 
   // deletes a list item from the weekend list
   function weekendDelete(weekendId) {
-    setWeekendListData(weekendListData.filter((index) => index.id !== weekendId));
-    updateWeekendTask(weekendId)
+    setWeekendListData(
+      weekendListData.filter((index) => index.id !== weekendId)
+    );
+    updateWeekendTask(weekendId);
   }
 
-
   // update the star rating of a task
-  function updateStarRating({rating, taskId}) {
+  function updateStarRating({ rating, taskId }) {
     let found = false;
-    dayData.forEach(day => {
+    dayData.forEach((day) => {
       day.list.forEach((task, index) => {
         if (task.id === taskId) {
-          setDayData(() => [...dayData.slice(0, day.day - 1), {...day, list: [...day.list.slice(0, index), {...task, rating: rating}, ...day.list.slice(index + 1)]}, ...dayData.slice(day.day)])
+          setDayData(() => [
+            ...dayData.slice(0, day.day - 1),
+            {
+              ...day,
+              list: [
+                ...day.list.slice(0, index),
+                { ...task, rating: rating },
+                ...day.list.slice(index + 1),
+              ],
+            },
+            ...dayData.slice(day.day),
+          ]);
           found = true;
-          return
+          return;
         }
-      })
-      if (found) {return}
-    })
+      });
+      if (found) {
+        return;
+      }
+    });
   }
 
   // update if a task in the weekend
   // takes in the task id and toogles the weekend from true to false
   function updateWeekendTask(taskId) {
     let found = false;
-    dayData.forEach(day => {
+    dayData.forEach((day) => {
       day.list.forEach((task, index) => {
         if (task.id === taskId) {
-          setDayData(() => [...dayData.slice(0, day.day - 1), {...day, list: [...day.list.slice(0, index), {...task, weekend: !task.weekend}, ...day.list.slice(index + 1)]}, ...dayData.slice(day.day)])
+          setDayData(() => [
+            ...dayData.slice(0, day.day - 1),
+            {
+              ...day,
+              list: [
+                ...day.list.slice(0, index),
+                { ...task, weekend: !task.weekend },
+                ...day.list.slice(index + 1),
+              ],
+            },
+            ...dayData.slice(day.day),
+          ]);
           found = true;
-          return
+          return;
         }
-      })
-      if (found) {return}
-    })
+      });
+      if (found) {
+        return;
+      }
+    });
   }
 
   // update the completed state for a task
   function updateCompletedTask(taskId) {
     let found = false;
-    dayData.forEach(day => {
+    dayData.forEach((day) => {
       day.list.forEach((task, index) => {
         if (task.id === taskId) {
-          setDayData(() => [...dayData.slice(0, day.day - 1), {...day, list: [...day.list.slice(0, index), {...task, completed: !task.completed}, ...day.list.slice(index + 1)]}, ...dayData.slice(day.day)])
+          setDayData(() => [
+            ...dayData.slice(0, day.day - 1),
+            {
+              ...day,
+              list: [
+                ...day.list.slice(0, index),
+                { ...task, completed: !task.completed },
+                ...day.list.slice(index + 1),
+              ],
+            },
+            ...dayData.slice(day.day),
+          ]);
           found = true;
-          return
+          return;
         }
-      })
-      if (found) {return}
-    })
+      });
+      if (found) {
+        return;
+      }
+    });
   }
 
   // update the comment made for a specific task to the state
-  function updateTaskComment({taskId, comment}) {
+  function updateTaskComment({ taskId, comment }) {
     let found = false;
-    dayData.forEach(day => {
+    dayData.forEach((day) => {
       day.list.forEach((task, index) => {
         if (task.id === taskId) {
-          setDayData(() => [...dayData.slice(0, day.day - 1), {...day, list: [...day.list.slice(0, index), {...task, comment: comment}, ...day.list.slice(index + 1)]}, ...dayData.slice(day.day)])
+          setDayData(() => [
+            ...dayData.slice(0, day.day - 1),
+            {
+              ...day,
+              list: [
+                ...day.list.slice(0, index),
+                { ...task, comment: comment },
+                ...day.list.slice(index + 1),
+              ],
+            },
+            ...dayData.slice(day.day),
+          ]);
           found = true;
-          return
+          return;
         }
-      })
-      if (found) {return}
-    })
+      });
+      if (found) {
+        return;
+      }
+    });
   }
 
   // fetches data from the server based on the week selected from the dropdown
   async function getDayData() {
-    let response = await fetch(
-      `http://localhost:3000/api/v1/1/${input}`
-    );
+    let response = await fetch(`http://localhost:3000/api/v1/1/${weekNumber}`);
     let data = await response.json();
     setDayData(data.payload);
   }
 
   useEffect(() => {
     getDayData();
-  }, [input]);
+  }, [weekNumber]);
 
-  
   return (
     <div className="App">
-      <img className={imageVisibility} src="https://puu.sh/J7zIq/efe0cf5dad.png" alt="" />
+      <img
+        className={imageVisibility}
+        src="https://puu.sh/J7zIq/efe0cf5dad.png"
+        alt=""
+      />
       <Header />
       <main>
-      <div className='categories-container'>
-      <div className="categories-box">
-        <div className="categories">
-        <Dropdown  selected={selected} setSelected={setSelected} setInput={setInput}/>
-          <div className="categories-map">
-            {dayData.map((input) => {
-              return (
-                <Categories
-                  key={input.day + 1}
-                  id={input.day}
-                  dayListData={input.list}
-                  handleClick={handleClick}
-                />
-              );
-            })}
-          </div>
-          <WeekendButton weekendButtonClick={weekendButtonClick} />
-        </div>
-        </div>
-      </div>
+        <Categories
+          dayData={dayData}
+          updateWeekNumber={updateWeekNumber}
+          handleClick={handleClick}
+          weekendButtonClick={weekendButtonClick}
+        />
         <Todo
-          weekendClick={weekendAddClick}
           toDoClass={toDoClass}
           listData={dayData[dayId].list}
           resourceData={dayData[dayId].resources}
           weekendCheck={weekendCheck}
           weekendListData={dayData}
+          weekendClick={weekendAddClick}
           weekendListDelete={weekendDelete}
           updateStarRating={updateStarRating}
           updateCompletedTask={updateCompletedTask}
